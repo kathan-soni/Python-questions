b8325194142db5726c162575b41c579c8e5dcaeb
Kats <153934724+kathan-soni@users.noreply.github.com>
2024-08-30 15:02:35 +0530

    Update maximum_subarr.py

--- maximum_subarr.py
+++ maximum_subarr.py
@@ -4,6 +4,10 @@ def sub_arr_sum(arr):
 
     for i in range(1, len(arr)):
         max_ending_here = max(arr[i], max_ending_here + arr[i])
+        max_so_far = max(max_so_far, max_ending_here)
+    return max_so_far
+result = sub_arr_sum([1,2,3,-1,3])
+print(result)
         # here it wil take max value from current element or the element next to it or to the element we get after adding it
         
 
